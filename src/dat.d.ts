f740192baab41984a2f4ba387e79734502541aeb
vponce <vponce@wujitouch.com>
2020-10-15 20:37:16 -0400

    type defs

---
+++ src/dat.d.ts
@@ -0,0 +1,15 @@
+// declare var window: Window;
+// declare module "three-dat.gui";
+
+declare namespace dat {
+  export interface GUI {
+    addFog(name: string, light: THREE.Fog): void;
+    addFogExp2(name: string, light: THREE.FogExp2): void;
+    addLight(name: string, light: THREE.Light): void;
+    addMaterial(name: string, material: THREE.Material): void;
+    addMesh(name: string, mesh: THREE.Mesh): void;
+    addObject3D(name: string, mesh: THREE.Object3D): void;
+    addScene(name: string, scene: THREE.Scene): void;
+    addVector(name: string, vector: THREE.Vector): void;
+  }
+}
--- src/global.d.ts
+++ src/global.d.ts
@@ -15,24 +15,7 @@ declare global {
 }
 
 declare namespace NodeJS {
-  // @ts-ignore
   export interface ProcessEnv {
     apiUrl: string;
-  }
-}
-
-// declare var window: Window;
-// declare module "three-dat.gui";
-
-declare module dat {
-  export interface GUI {
-    addFog(name: string, light: THREE.Fog): void;
-    addFogExp2(name: string, light: THREE.FogExp2): void;
-    addLight(name: string, light: THREE.Light): void;
-    addMaterial(name: string, material: THREE.Material): void;
-    addMesh(name: string, mesh: THREE.Mesh): void;
-    addObject3D(name: string, mesh: THREE.Object3D): void;
-    addScene(name: string, scene: THREE.Scene): void;
-    addVector(name: string, vector: THREE.Vector): void;
   }
 }
